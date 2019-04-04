a49655f1007f3f5b04b9940e1a1ae897afc5ca07
Student <student@Makerss-MacBook-Pro.local>
2019-04-04 16:17:11 +0100

    throws an error if you try to release a bike and none are docked

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -12,14 +11,29 @@ describe DockingStation do
       expect(subject.dock(bike)).to eq bike
     end
 
+    describe '#release_bike' do
+      it "throws 'no bike available' error when there is no bike" do
+        expect{ subject.release_bike }.to raise_error("No bikes available")
+      end
+
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+    end
+
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
 
-    it 'releases working bikes' do
+    it 'releases a working bike' do
+        bike = Bike.new
+        subject.dock(bike)
         bike = subject.release_bike
         expect(bike).to be_working
     end
+
 end
